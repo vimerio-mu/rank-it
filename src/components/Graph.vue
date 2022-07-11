6c9ea26806d3963166fc3b62292a8e15a84b7290
zebin chen <498160912@qq.com>
2022-07-11 20:39:54 +0800

    feat:增加色卡配色

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -167,6 +167,26 @@ const colorSwatch = reactive({
       name: '彩虹',
       colors: ['#f87171','#fb923c','#fde047','#a3e635','#2dd4bf']
     },
+    {
+      name: '黑白',
+      colors: ['#2F3542','#57606F','#747D8C','#A4B0BE','#CED6E0']
+    },
+    {
+      name: '一直游到海水变蓝',
+      colors: ['#4B72A4','#5587BC','#6997C1','#94B8D5','#B4CAE1']
+    },
+    {
+      name: '马卡龙',
+      colors: ['#FEB1BB','#E6C8F8','#CAC9F8','#BCEAE1','#CEFADE']
+    },
+    {
+      name: '布达佩斯大饭店',
+      colors: ['#E7768C','#F9A3B7','#FDC2C4','#D0A1A9','#AC7382']
+    },
+    {
+      name: '莫兰迪',
+      colors: ['#8895A5','#A0A7B9','#BFCBD9','#B6C4B3','#8DA48D']
+    }
   ]
 })
 
